43b4dce336668d36b9fd97cf7d0cab9fdd712b0f
Rakshit Bhardwaj <100684015+rakshit-bhardwaj@users.noreply.github.com>
2023-06-04 23:09:17 +0530

    Update matrixbinarysearch.cpp

--- matrixbinarysearch.cpp
+++ matrixbinarysearch.cpp
@@ -2,14 +2,18 @@ bool searchMatrix(vector<vector<int>>& mat, int target) {
     int n = mat.size();
     int m = mat[0].size();
     int x=-1;
-    for(int i = 0;i<n;i++){
-        if(target>=mat[i][0] && target<=mat[i][m-1]){
-            x = i;
+    int s = 0,e = n-1;
+    while(s <= e){
+        int mid = (s+e)/2;
+        if(target>=mat[mid][0] && target<=mat[mid][m-1]){
+            x = mid;
             break;
         }
+        if(target < mat[mid][0]) e = mid - 1;
+        else s = mid + 1;
     }
     if(x==-1) return false;
-    int s = 0,e = m-1;
+    s = 0;e = m-1;
     while(s<=e){
         int mid = (s+e)/2;
         if(mat[x][mid]==target) return true;
