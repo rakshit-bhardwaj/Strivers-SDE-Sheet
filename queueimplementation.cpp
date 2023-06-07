7ef3ed1b12cadd8ee74fd60ff391a60e98973ffb
Rakshit Bhardwaj <100684015+rakshit-bhardwaj@users.noreply.github.com>
2023-06-07 18:08:22 +0530

    Update queueimplementation.cpp

--- queueimplementation.cpp
+++ queueimplementation.cpp
@@ -1,1 +1,30 @@
+#include <bits/stdc++.h> 
+class Queue {
+public:
+    vector<int> q;
+    int cur=0,fr=0,size=0;
+    Queue() {
+        q.resize(5000);
+    }   
 
+    /*----------------- Public Functions of Queue -----------------*/
+    bool isEmpty() {
+        return size==0;
+    }
+
+    void enqueue(int data) {
+        size++;
+        q[cur++] = data;
+    }
+
+    int dequeue() {
+        if(size==0) return -1;
+        size--;
+        return q[fr++];
+    }
+
+    int front() {
+        if(size==0) return -1;
+        return q[fr];
+    }
+};
