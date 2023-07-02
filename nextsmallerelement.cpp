fb6209dced5332f5caeb39403817638f8bb53938
Rakshit Bhardwaj <100684015+rakshit-bhardwaj@users.noreply.github.com>
2023-07-03 00:26:57 +0530

    Create nextsmallerelement.cpp

---
+++ nextsmallerelement.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> nextSmallerElement(vector<int> &arr, int n)
+{
+    stack<int> s;
+    vector<int> ans;
+    for(int i =n-1;i>=0;i--){
+        while(!s.empty() && s.top() >= arr[i]) s.pop();
+        if(s.empty()) ans.push_back(-1);
+        else ans.push_back(s.top());
+        s.push(arr[i]);
+    }
+    reverse(ans.begin(),ans.end());
+    return ans;
+}
