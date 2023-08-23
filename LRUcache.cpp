04d05dc9ff7726ddd705893e780be11c061933f4
Rakshit Bhardwaj <100684015+rakshit-bhardwaj@users.noreply.github.com>
2023-08-23 17:16:42 +0530

    Update LRUcache.cpp

--- LRUcache.cpp
+++ LRUcache.cpp
@@ -4,48 +4,52 @@ using namespace std;
 class LRUCache
 {
 public:
-    unordered_map<int, list<pair<int, int>>::iterator> m;
+    unordered_map<int,list<pair<int,int>>::iterator> m;
     list<pair<int,int>> l;
-    int cur = 0,cap;
+    int size,cur=0;
 
     LRUCache(int capacity)
     {
-        cap = capacity;
+        size = capacity;
     }
 
     int get(int key)
     {
         if(m.find(key)==m.end()) return -1;
-        int value = m[key]->second;
-        l.push_back({key,value});
+        int ans = m[key]->second;
         l.erase(m[key]);
+        m.erase(key);
+        l.push_back({key,ans});
         auto it = l.end();
         it--;
         m[key] = it;
-        return value;
+        return ans;
     }
 
     void put(int key, int value)
-    {   
-        
-        if(m.find(key)==m.end()){
-            if(cur == cap){
-                auto fr = l.front();
-                m.erase(fr.first);
-                l.pop_front();
-                cur--;
-            }
-            l.push_back({key,value});
-            auto it = l.end();
-            it--;
-            m[key] = it;
-            cur++;
-        } else {
+    {
+        if(m.find(key)!=m.end()){
             l.erase(m[key]);
             l.push_back({key,value});
+            m.erase(key);
             auto it = l.end();
             it--;
             m[key] = it;
+        } else {
+            if(cur < size){
+                l.push_back({key,value});
+                auto it = l.end();
+                it--;
+                m[key] = it;
+                cur++;
+            } else {
+                m.erase(l.front().first);
+                l.pop_front();
+                l.push_back({key,value});
+                auto it = l.end();
+                it--;
+                m[key] = it;
+            }
         }
     }
 };
